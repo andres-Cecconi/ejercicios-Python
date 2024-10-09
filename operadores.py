3008ea5556edc6bf33369294f77162e74a606650
Andres Cecconi <acecconi@uade.edu.ar>
2024-10-09 17:39:04 -0300

    tipos de operadores en Python

---
+++ operadores.py
@@ -0,0 +1,96 @@
+""" OPERADORES """
+
+# Operadores aritméticos
+
+#suma
+print(f"Suma 10 + 5 = {10 + 5}")
+suma = 10 + 5
+print(suma)
+#resta
+print(f"Resta 10 -5 = {10 -5}")
+resta = 10 - 5
+print(resta)
+#multiplicación
+print(f"Multiplicación 10 * 5 = {10 * 5}")
+multiplicacion = 10 * 5
+print(multiplicacion)
+#división
+print(f"División 10 / 5 = {10 / 5}")
+division = 10 / 5
+print(division)
+#módulo
+print(f"Módulo 10 % 5 = {10 % 5}")
+modulo = 10 % 5
+print(modulo)
+#exponencial
+print(f"Exponencial 10 ** 5 = {10 ** 5}")
+exponencial = 10 ** 5
+print(exponencial)
+
+
+
+# Operadores de comparación
+
+#igual
+print(f"10 == 5 = {10 == 5}")
+igual = 10 == 5
+print(igual)
+#diferente
+print(f"10 != 5 = {10 != 5}")
+diferente = 10 != 5
+print(diferente)
+#mayor
+print(f"10 > 5 = {10 > 5}")
+mayor = 10 > 5
+print(mayor)
+#menor
+print(f"10 < 5 = {10 < 5}")
+menor = 10 < 5
+print(menor)
+#mayor o igual
+print(f"10 >= 5 = {10 >= 5}")
+#menor o igual
+print(f"10 <= 5 = {10 <= 5}")
+
+#operadores logicos
+
+#and
+print(f" AND &&: 10 + 3 == 13 and 5 - 1 == 4 = {10 + 3 == 13 and 5 - 1 == 4}")
+#or
+print(f" OR ||: 10 + 3 == 13 or 5 - 1 == 4 = {10 + 3 == 13 or 5 - 1 == 4}")
+#not
+print(f" NOT !: not 10 + 3 == 14 = {not 10 + 3 == 14}")
+
+#operadores de asignación
+#asignación
+my_number = 10
+print(my_number)
+#suma y asignación
+my_number +=5
+print(my_number)
+#resta y asignación
+my_number -=5
+print(my_number)
+#multiplicación y asignación
+my_number *=5
+print(my_number)
+#división y asignación
+my_number /=5
+print(my_number)
+#modulo y asignación
+my_number %=5
+print(my_number)
+#exponencial y asignación
+my_number **=5
+print(my_number)
+#division entera y asignación
+my_number //=5
+print(my_number)
+
+
+#operadores de pertenencia
+#in
+print(f"10 in [1,2,3,4,5] = {10 in [1,2,3,4,5]}")
+#not in
+print(f"10 not in [1,2,3,4,5] = {10 not in [1,2,3,4,5]}")
+
